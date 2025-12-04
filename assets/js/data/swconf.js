40d9d8d472394f32ce214788bde620de55105795
tibane0 <nkatekotibane101@gmail.com>
2025-12-04 19:33:33 +0200

    Deploying notes and blog updates: 2025-12-04 19:33:29

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1764869613',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/projects/',
+      
+        '/resume/',
+      
+        '/tags/',
+      
+        '/categories/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
