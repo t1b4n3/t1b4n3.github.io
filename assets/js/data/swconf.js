88c1aace82b031bb9f868e0c0e65889e8395d704
Nkateko TIbane <nkatekotibane101@gmail.com>
2026-02-21 10:05:03 +0200

    Deploying notes and blog updates: 2026-02-21 10:04:40

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1771661101',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/projects/',
+      
+        '/tags/',
+      
+        '/categories/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
